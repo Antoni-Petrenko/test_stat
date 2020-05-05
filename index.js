8638ceb7771e959cd5d9c0b3f91e5b3acf4f7912
Петренко Антон Володимирович <petrenko_av@steko.net>
2020-05-05 13:53:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static(path.join(__dirname, "public"))); // share public folder
+
+app.get('/', function (request, response) {
+    response.send(`
+    <h1>Steko stat</h1>
+    `);
+});
+
+app.post('/', bodyParser, (req, res, next) => {
+    console.log(req.body)
+})
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
